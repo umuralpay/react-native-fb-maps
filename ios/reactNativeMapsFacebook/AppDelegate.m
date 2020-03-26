579ca17cdf8abe17cb5689fb9134b900257e5d3d
Umur Alpay <umuralpay@outlook.com>
2020-03-26 14:00:59 +0300

    google maps implementation

--- ios/reactNativeMapsFacebook/AppDelegate.m
+++ ios/reactNativeMapsFacebook/AppDelegate.m
@@ -10,11 +10,12 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import <GoogleMaps/GoogleMaps.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"123"];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"reactNativeMapsFacebook"
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
-import { View, Text } from "react-native";
+import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import MapView from 'react-native-maps';
 
 class MapScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text>MapScreen</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1, backgroundColor: "#000"}}>
+        <View style={styles.container}>
+          <MapView // remove if not using Google Maps
+            style={styles.map}
+            region={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.015,
+              longitudeDelta: 0.0121,
+            }}></MapView>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
-export default MapScreen
+export default MapScreen;
